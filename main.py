8812c5f2186735899ed1049d773605e576d51608
Vladimir Semenov <33404130+subatiq@users.noreply.github.com>
2023-02-11 12:46:47 +0300

    Battle-tested rewrite

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import time
-from subprocess import check_output
+from typing import Dict
+from subprocess import check_output, CalledProcessError
 
-def run(config: dict[str, str]) -> None:
-    print("Dropper plugin running")
-    print("Config: ", config)
-
+def run(config: Dict[str, str]) -> None:
     target = config.get('target')
     ip = config.get('ip')
     unavailable_seconds = int(config.get('unavailable_seconds', 10))
-    available_seconds = int(config.get('available_seconds', 10))
-    print('Installing iptables...')
-    output = check_output(f'docker exec {target} apt-get install iptables -y', shell=True)
-    print(output)
 
-    while True:
-        output = check_output(f"docker exec {target} bash -c 'iptables -C OUTPUT -d {ip} -j DROP || iptables -I OUTPUT -d {ip} -j DROP'", shell=True)
+    print('Checking if iptables are available...')
+    try:
+        check_output(f'docker exec {target} apt-get install iptables -y', shell=True)
+    except CalledProcessError as e:
+        print("Problem while trying to install iptables:", e)
+        return
+
+    try:
+        check_output(f"docker exec {target} bash -c 'iptables -C OUTPUT -d {ip} -j DROP || iptables -I OUTPUT -d {ip} -j DROP'", shell=True)
         print(f'--- {ip} is unavailable now for {target}. Switching in {unavailable_seconds} sec')
-        time.sleep(unavailable_seconds)
-        output = check_output(f"docker exec {target} bash -c 'iptables -D OUTPUT -d {ip} -j DROP'", shell=True)
-        print(f'+++ {ip} is available now for {target}. Switching in {available_seconds} sec')
-        time.sleep(available_seconds)
+    except Exception as e:
+        print("Can't turn off connection, probably container is dead", e)
+
+    time.sleep(unavailable_seconds)
+
+    try:
+        check_output(f"docker exec {target} bash -c 'iptables -D OUTPUT -d {ip} -j DROP'", shell=True)
+        print(f'+++ {ip} is available now for {target}. Switching off in the next run')
+    except Exception as e:
+        print("Can't turn on connection, probably container is dead", e)
